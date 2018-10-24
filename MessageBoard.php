b3f36e6672930d3315189e03c7ed31a0c5565b11
Unknown <AThompson027@west-mec.org>
2018-10-24 13:44:54 -0700

    Fixed bugs
    Fixed Bugs

--- MessageBoard.php
+++ MessageBoard.php
@@ -15,11 +15,36 @@
             $messageArray = file("messages.txt");
             switch ($_GET['action']) {
                 case 'Delete First':
+                    // this takes out the first element and shifts
                     array_shift($messageArray);
+                    break;
+                    // this pops out the last element from the array
+                case 'Delete Last':
+                    array_pop($messageArray);
+                    break;
+                case 'Delete Message':
+                     if (isset($_GET['message'])) {
+                         // this function takes out and element and replaces it with another element
+                         array_splice($messageArray, $_GET['message'], 1);
+                     }
+                    break;
+                case 'Remove Duplicates':
+                    $messageArray = array_unique($messageArray);
+                    $messageArray = array_values($messageArray);
                     break;
             }
             if (count($messageArray > 0)) {
-                echo "There are remaining messages in the array.<br>"; //debug 
+                $newMessages = implode($messageArray);
+                // write & binary 
+                // this will wipe out the content in the file
+                $fileHandle = fopen('messages.txt', "wb");
+                // if the file will not open or wipe out the content from the file then it will echo out an error message
+                if (!$fileHandle) {
+                    echo "There was an error updating the message file.\n";
+                } else {
+                    fwrite($fileHandle, $newMessages);
+                    fclose($fileHandle);
+                }
             } else {
                 // this unlinks the message.txt
                 unlink("messages.txt");
@@ -35,20 +60,34 @@
         echo "<table style=\"background-color: lightgrey\" border=\"1\" width=\"100%\">\n";
         // counts how many elements are in the array
         $count = count($messageArray);
-        for ($i=0; $i < $count; $i++) {
-            // this breaks the string every time there is an "~" into multiple strings
+        // this will create a shelf in the array with the subject as the key
+       
+
+        for ($i = 0; $i < $count; $i++) {
             $currMsg = explode("~", $messageArray[$i]);
+            $keyMessageArray[$currMsg[0]] = $currMsg[1] . "~" . $currMsg[2];
+        }
+
+        $index = 1;
+        $key = key($keyMessageArray);
+        foreach ($keyMessageArray as $message) {
+            // this breaks the string every time there is an "~" into multiple strings
+            $currMsg = explode("~", $message);
             echo "<tr>\n";
             
-            echo "<td width=\"5%\" style=\"text-align: center; font-weight: bold\">" . ($i + 1) . "</td>\n";
+            echo "<td width=\"5%\" style=\"text-align: center; font-weight: bold\">" . $index . "</td>\n";
             
-            echo "<td width=\"95%\"><span style=\"font-weight: bold\">Subject: </span>" . htmlentities($currMsg[0]) . "<br>\n";
+            echo "<td width=\"85%\"><span style=\"font-weight: bold\">Subject: </span>" . htmlentities($key) . "<br>\n";
             
-            echo "<span style=\"font-weight: bold\">Name: </span>" . htmlentities($currMsg[1]) . "<br>\n";
+            echo "<span style=\"font-weight: bold\">Name: </span>" . htmlentities($currMsg[0]) . "<br>\n";
             
-            echo "<span style=\"text-decoration: underline; font-weight: bold\">Message: </span><br>\n" . htmlentities($currMsg[2]) . "</td>\n";
-            
+            echo "<span style=\"text-decoration: underline; font-weight: bold\">Message: </span><br>\n" . htmlentities($currMsg[1]) . "</td>\n";
+            echo "<td width=\"10%\" style=\"text-align:center\">" . "<a href='MessageBoard.php?" . "action=Delete%20Message&" . "message=" . ($index - 1) . "'>" . "Delete This Message</a></td>\n";
             echo "</tr>\n";
+            ++$index;
+            //gets the next key
+            next($keyMessageArray);
+            $key = key($keyMessageArray);
         }
         echo "</table>\n";
     }
@@ -56,7 +95,9 @@
     <p>
     <a href="PostMessage.php">Post New Message</a><br>
 <!--part of the URL that passes data in named value pairs (seperated by "&")-->
-    <a href="MessageBoard.php?action=Delete%20First">Delete First Message</a>
+    <a href="MessageBoard.php?action=Delete%20First">Delete First Message</a><br>
+    <a href="MessageBoard.php?action=Delete%20Last">Delete Last Message</a><br>
+    <a href="MessageBoard.php?action=Remove%20Duplicates">Remove Duplicates</a><br>
     </p>
 </body>
 </html>
